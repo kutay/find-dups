1e71bbb0e66f7a1306c2d5eb3806bc9e4b874273
Aykut Açikel <aykut.acikel@gmail.com>
2020-05-25 21:10:00 +0200

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,26 @@ use std::{fs, io};
 
 use rayon::prelude::*;
 
+use chrono::{DateTime, Local, Utc};
 use log::{info, trace, warn};
+use std::time::{Instant, SystemTime};
+
+#[derive(Debug)]
+struct FileEntry {
+    path: String,
+    hash: String,
+    created: String,
+}
 
 fn hash_digest(filepath: &Path) -> String {
-    trace!("Hashing    : {:?}", filepath);
+    trace!("Hashing file {:?}", filepath);
+
     let mut file = fs::File::open(filepath).unwrap();
     let mut hasher = Blake2b::new();
     let n = io::copy(&mut file, &mut hasher).unwrap();
-    let hash = hasher.result();
+    trace!("Bytes processed: {}", n);
 
-    // println!("Bytes processed: {}", n);
-
-    format!("{:x}", hash)
+    format!("{:x}", hasher.result())
 }
 
 fn is_hidden(entry: &DirEntry) -> bool {
@@ -33,7 +41,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn walk_folder(dirpath: &str) {
+fn walk_folder(dirpath: &str) -> HashMap<String, Vec<FileEntry>> {
     let mut filenames = HashMap::new();
 
     let mut files = vec![];
@@ -44,32 +52,40 @@ fn walk_folder(dirpath: &str) {
         .filter_map(Result::ok)
         .filter(|e| !e.file_type().is_dir() && !e.path_is_symlink())
     {
-        files.push(String::from(entry.path().to_str().unwrap()));
+        // files.push(String::from(entry.path().to_str().unwrap()));
+        files.push((
+            String::from(entry.path().to_str().unwrap()),
+            format_system_time(entry.metadata().unwrap().created().unwrap()),
+        ));
     }
 
     // Compute hashes
-    let files_with_hashes: Vec<(String, String)> = files
+    let files_with_hashes: Vec<FileEntry> = files
         .par_iter()
         .map(|f| {
-            let hash = hash_digest(Path::new(&f));
-            (f.to_string(), hash)
+            let hash = hash_digest(Path::new(&f.0));
+
+            FileEntry {
+                path: f.0.to_string(),
+                hash,
+                created: f.1.clone(),
+            }
         })
         .collect();
     trace!("Result    : {:?}", files_with_hashes);
 
     // Group files with same hashes
-    for file_with_hash in files_with_hashes {
-        let counter = filenames.entry(file_with_hash.1).or_insert(vec![]);
-        counter.push(file_with_hash.0);
+    for file_entry in files_with_hashes {
+        let counter = filenames.entry(file_entry.hash.clone()).or_insert(vec![]);
+        counter.push(file_entry);
     }
 
-    // Print
-    for filename in filenames {
-        if filename.1.len() > 1 {
-            info!("Key        : {} / {}", filename.1.len(), filename.0);
-            info!("Values     : {:?}", filename.1);
-        }
-    }
+    filenames
+}
+
+fn format_system_time(systime: SystemTime) -> String {
+    let datetime: DateTime<Local> = systime.into();
+    format!("{}", datetime.format("%Y-%m-%d %T"))
 }
 
 fn main() {
@@ -80,5 +96,15 @@ fn main() {
         panic!("Please provide a path");
     }
 
-    walk_folder(&args[1]);
+    let grouped_files = walk_folder(&args[1]);
+
+    // Print
+    for grouped in &grouped_files {
+        if grouped.1.len() > 1 {
+            info!("Found file with {} duplicates", grouped.1.len());
+            for dup in grouped.1 {
+                info!("    - {:?}     {:?}", dup.path, dup.created);
+            }
+        }
+    }
 }
