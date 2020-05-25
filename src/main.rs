9cb9273f2365ac0b0ffe8170014e351f33313438
Aykut Açikel <aykut.acikel@gmail.com>
2020-05-25 14:44:16 +0200

    Compute hash of file content to find duplicates

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,31 @@ extern crate walkdir;
 
 use std::collections::HashMap;
 use std::error::Error;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
+
+use blake2::{Blake2b, Digest};
+use std::path::Path;
+use std::{fs, io};
+
+fn hash_digest(filepath: &Path) -> String {
+    println!("Hashing    : {:?}", filepath);
+    let mut file = fs::File::open(filepath).unwrap();
+    let mut hasher = Blake2b::new();
+    let n = io::copy(&mut file, &mut hasher).unwrap();
+    let hash = hasher.result();
+
+    // println!("Bytes processed: {}", n);
+
+    format!("{:x}", hash)
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
 
 fn walk_folder(dirpath: &str) {
     let mut filenames = HashMap::new();
@@ -10,10 +34,14 @@ fn walk_folder(dirpath: &str) {
     for entry in WalkDir::new(dirpath)
         .into_iter()
         .filter_map(Result::ok)
-        .filter(|e| !e.file_type().is_dir())
+        .filter(|e| !e.file_type().is_dir() && !e.path_is_symlink())
     {
         let f_name = String::from(entry.file_name().to_string_lossy());
-        let counter = filenames.entry(f_name.clone()).or_insert(vec![]);
+
+        let hash = hash_digest(&entry.path());
+        let key = hash;
+
+        let counter = filenames.entry(key).or_insert(vec![]);
         counter.push(String::from(entry.path().to_str().unwrap()));
     }
 
@@ -24,5 +52,6 @@ fn walk_folder(dirpath: &str) {
 }
 
 fn main() {
-    walk_folder("/home/aykut/Documents/find-dupes");
+    walk_folder("/home/aykut/Documents");
+    // walk_folder("/home/aykut/Documents/find-dupes");
 }
